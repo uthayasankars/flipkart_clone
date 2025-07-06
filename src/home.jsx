63f8938409c18ece9af2ef12f52902806a7c81bc
uthayasankars <uthaya777sankar@gmail.com>
2025-07-06 15:49:10 +0530

    datasjs

--- src/home.jsx
+++ src/home.jsx
@@ -3,10 +3,11 @@ import Category from './components/Category/category';
 import { useEffect, useState } from "react";
 
 function Home(props) {
-  const [name, setName] = useState("Varun")
-  useEffect(()=>{
-    console.log(`Home mounted with name: ${name}`)
-  },[])
+  const [name, setName] = useState("Uthayasankar S")
+  useEffect(() => {
+    console.log(`Home mounted with name: ${name}`);
+  },[name]);
+  
   return (
     <>
     <div className="main-home">
@@ -16,6 +17,7 @@ function Home(props) {
     <div className="home-category shadow-2x1">
     <Category/>
     </div>
+    <input type="text" name="userName" id="userName" placeholder="enter name" onChange={() => setName(document.getElementById('userName').value)}/>
     </div>
     </>
 
