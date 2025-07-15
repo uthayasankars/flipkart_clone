b786a235b38740097ce32114bef6b1eec2a36adf
uthayasankars <uthaya777sankar@gmail.com>
2025-07-15 11:28:18 +0530

    useRef update

--- src/components/Hooks/ref.jsx
+++ src/components/Hooks/ref.jsx
@@ -1,14 +1,26 @@
 import React, { useRef } from 'react'
+import audio from '../Hooks/audio.mp3'
 
 function References() {
-  let inpRef = useRef()
-  let handleRef = ()=>{
 
-  }
+  let inpRef = useRef()
+
+  let audioRef = useRef()
+
   return (
     <>
     <input ref={inpRef} type="text" name="name" id="name" placeholder='enter something...' />
-    <button onClick={inpRef.current.focus()}>click ti focus</button>
+    {/* <button onClick={()=> inpRef.current.focus()}>click to focus</button> */}
+
+   
+
+    <button onClick={()=> inpRef.current.value="Enjoy Your Day"}>click to focus</button><br />
+
+    <audio ref={audioRef} src={audio} controls></audio>
+
+    <button onClick={()=> audioRef.current.play()}>Play</button><br />
+    <button onClick={()=> audioRef.current.pause()}>Pause</button>
+
     </>
   )
 }
